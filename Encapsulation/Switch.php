994497c7275f58aeccbd62e6f4acf068332b96ab
alexbonavila <alexbonavila@iesebre.com>
2015-10-05 14:50:13 +0200

    Exercici Encapsulation

---
+++ Encapsulation/Switch.php
@@ -0,0 +1,23 @@
+<?php
+
+class Switc {
+    public function on(){
+
+    }
+
+    public function off(){
+
+    }
+
+    private function connect(){
+
+    }
+
+    private function activate(){
+
+    }
+
+}
+
+$switc = new Switc;
+$switc->connect();
