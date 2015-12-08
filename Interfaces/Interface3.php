654c146bf9e9976d466af25c0cd8ba52825fd5e8
alexbonavila <alexbonavila@iesebre.com>
2015-12-08 14:30:18 +0100

    ara a anat fent una serie de demostracions i explcacions e anat intentant compmrendre el que feia i copiant

--- Interfaces/Interface3.php
+++ Interfaces/Interface3.php
@@ -1,2 +1,27 @@
 <?php
 
+interface CanBeFiltred{
+    public function filter();
+
+}
+
+class Favorited implements CanBeFiltred{
+    public function filter()
+    {
+
+    }
+}
+
+class Unwatched implements CanBeFiltred{
+    public function filter()
+    {
+
+    }
+}
+
+class Difficulty implements CanBeFiltred{
+    public function filter()
+    {
+
+    }
+}
