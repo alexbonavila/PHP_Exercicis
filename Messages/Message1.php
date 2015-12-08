5df70cd72a1ebeb3a0468a8f31f8b86e5ac2fdc0
alexbonavila <alexbonavila@iesebre.com>
2015-12-08 14:54:26 +0100

    video finalitzat he anat copiant i comprenent el que feia

--- Messages/Message1.php
+++ Messages/Message1.php
@@ -34,13 +34,50 @@ class Business{
         // add $person to staff collection
         $this->staff->add($person);
     }
+
+    public function getStaffMembers()
+    {
+        return $this->staff->members();
+    }
+    
 }
 
 class Staff{
 
+    protected $members =[];
+
+    /**
+     * Staff constructor.
+     * @param array $members
+     */
+    public function __construct($members=[])
+    {
+        $this->members = $members;
+    }
+
+
     public function add(Person $person){
 
-        
+        $this->members[]=$person;
 
     }
+
+    public function members()
+    {
+        return $this->members;
+    }
 }
+
+$jeffrey = new Person('Jeffrey Way');
+
+$staff = new Staff([$jeffrey]);
+
+$laracasts = new Business($staff);
+
+//$laracasts->hire($jeffrey);
+
+$laracasts->hire(new Person('Jane Doe'));
+
+//var_dump($staff);
+
+var_dump($laracasts->getStaffMembers());
